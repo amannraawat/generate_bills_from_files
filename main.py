e498de9f034dfb51f999ac2805ecc76ba9c4663b
Aman Rawat <amannraawat@gmail.com>
2024-02-13 13:38:10 +0530

    added headers to the pdf

--- main.py
+++ main.py
@@ -7,32 +7,49 @@ from pathlib import Path
 filepaths = glob.glob('data/*.xlsx')
 
 for filepath in filepaths:
-    data = pd.read_excel(filepath, sheet_name='Sheet1')
-    # print(data)
     
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
+    
+    #separating filenames
     filename = Path(filepath).stem
     # print(filename)
     
+    #adding invoice name and number on the pdf
     invoice_no = filename.split("-")[0]
     pdf.set_font(family="Arial", size=16, style="I")
     pdf.cell(w=50, h=8, txt=f"Invoice No.{invoice_no}", ln=1)
     
+    #adding date on the invoice pdf
     date = filename.split("-")[1]
     pdf.set_font(family="Times", size=16, style="B")
     pdf.cell(w=50, h=8, txt=f"Date:{date}", ln=2)
     
-    #for adding rows to the pdf
+    #reading data from the excel file
     data = pd.read_excel(filepath, sheet_name='Sheet1')
     # print(data)
+    
+    
+    #adding headers(product_id, name..) from the excel files
+    columns_name = list(data.columns)
+    # print(columns_name)
+    pdf.set_font(family="Times", size=10)
+    pdf.set_text_color(80,80,80)
+    pdf.cell(w=30, h=8, txt=columns_name[0], border=1)
+    pdf.cell(w=25, h=8, txt=columns_name[1], border=1)
+    pdf.cell(w=50, h=8, txt=columns_name[2], border=1)
+    pdf.cell(w=30, h=8, txt=columns_name[3], border=1)
+    pdf.cell(w=20, h=8, txt=columns_name[4], border=1, ln=1)
+    
+    
+    #for adding rows to the pdf
     for index, row in data.iterrows():
         pdf.set_font(family="Times", size=10)
         pdf.set_text_color(80,80,80)
         pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1)
-        pdf.cell(w=50, h=8, txt=str(row["product_name"]), border=1)
-        pdf.cell(w=30, h=8, txt=str(row["amount_purchased(in kg/quantity)"]), border=1)
-        pdf.cell(w=20, h=8, txt=str(row["price(per unit)"]), border=1)
+        pdf.cell(w=25, h=8, txt=str(row["product_name"]), border=1)
+        pdf.cell(w=50, h=8, txt=str(row["amount_purchased(in kg/quantity)"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["price(per unit)"]), border=1)
         pdf.cell(w=20, h=8, txt=str(row["total_price"]), border=1, ln=1)
 
     pdf.output(f"Invoices/{filename}.pdf")
