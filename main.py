c10b903e83ef506132f4aa23d312ace68bb01704
Aman Rawat <amannraawat@gmail.com>
2024-02-13 13:05:40 +0530

    added rows data from excel files

--- main.py
+++ main.py
@@ -16,12 +16,24 @@ for filepath in filepaths:
     # print(filename)
     
     invoice_no = filename.split("-")[0]
-    pdf.set_font(family="Times", size=16, style="B")
+    pdf.set_font(family="Arial", size=16, style="I")
     pdf.cell(w=50, h=8, txt=f"Invoice No.{invoice_no}", ln=1)
     
     date = filename.split("-")[1]
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"Date:{date}")
+    pdf.cell(w=50, h=8, txt=f"Date:{date}", ln=2)
+    
+    #for adding rows to the pdf
+    data = pd.read_excel(filepath, sheet_name='Sheet1')
+    # print(data)
+    for index, row in data.iterrows():
+        pdf.set_font(family="Times", size=10)
+        pdf.set_text_color(80,80,80)
+        pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1)
+        pdf.cell(w=50, h=8, txt=str(row["product_name"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["amount_purchased(in kg/quantity)"]), border=1)
+        pdf.cell(w=20, h=8, txt=str(row["price(per unit)"]), border=1)
+        pdf.cell(w=20, h=8, txt=str(row["total_price"]), border=1, ln=1)
 
     pdf.output(f"Invoices/{filename}.pdf")
     
